3927a4070f917562898f0daa5301fce708048f15
anwarmaswadeh <anwarmaswadeh77@gmail.com>
2024-11-26 09:00:07 +0200

    test

--- test_mycode.py
+++ test_mycode.py
@@ -3,7 +3,7 @@ import pytest
 from mycode import add_numbers
 
 def test_add_positive():
-    assert add_numbers(3, 5) == 5
+    assert add_numbers(3, 5) == 8
 
 def test_add_zero():
     assert add_numbers(0, 5) == 5
